d60e75fec51cedb2f34aa093bae59b329275cada
Latkos <48084189+Latkos@users.noreply.github.com>
2023-02-08 00:17:12 +0100

    Small bug fix

--- new_named_entity/named_entity_utility_functions.py
+++ new_named_entity/named_entity_utility_functions.py
@@ -16,7 +16,7 @@ def create_dataset_from_dataframe(df):
     return dataset
 
 def preprocess_dataframe(df, label_mapping):
-    df['tokens'], df['labels'] = zip(*df['text'].map(create_tokens_and_labels))
+    df['tokens'], df['labels'] = zip(*df['text'].map(create_tokens_and_labels_for_two_entities))
     df['ner_tags'] = df['labels'].apply(lambda x: [label_mapping[i] for i in x])
     return df
 
@@ -39,7 +39,7 @@ def extract_tokens_and_create_labels(tokens, special_elements):
     return new_tokens, labels
 
 
-def create_tokens_and_labels(text):
+def create_tokens_and_labels_for_two_entities(text):
     replacement_map = {"<e1>": " StartEntity1 ", "</e1>": " StopEntity1 ", "<e2>": " StartEntity2 ",
                        "</e2>": " StopEntity2 "}
     for word, replacement in replacement_map.items():
@@ -56,26 +56,3 @@ def split_dataset(ds, split=0.2):
     train = split_ds["train"]
     val = split_ds["test"]
     return train, val
-
-    def tokenize_adjust_labels(self, all_samples_per_split):
-        tokenized_samples = self.tokenizer.batch_encode_plus(all_samples_per_split["tokens"], is_split_into_words=True)
-        total_adjusted_labels = []
-        for k in range(0, len(tokenized_samples["input_ids"])):
-            prev_wid = -1
-            word_ids_list = tokenized_samples.word_ids(batch_index=k)
-            existing_label_ids = all_samples_per_split["ner_tags"][k]
-            i = -1
-            adjusted_label_ids = []
-            for wid in word_ids_list:
-                if wid is None:
-                    adjusted_label_ids.append(-100)
-                elif wid != prev_wid:
-                    i = i + 1
-                    adjusted_label_ids.append(existing_label_ids[i])
-                    prev_wid = wid
-                else:
-                    adjusted_label_ids.append(existing_label_ids[i])
-
-            total_adjusted_labels.append(adjusted_label_ids)
-        tokenized_samples["labels"] = total_adjusted_labels
-        return tokenized_samples
