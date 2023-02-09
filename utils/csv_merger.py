5fed36eb409abdad3b959d7061a2e000d95db181
Latkos <48084189+Latkos@users.noreply.github.com>
2023-02-09 20:57:08 +0100

    Fix CSV merger

--- utils/csv_merger.py
+++ utils/csv_merger.py
@@ -1,12 +1,15 @@
 import shutil
 import glob
+import os
+import sys
 
 
 def merge_csv_files(path, pattern="*.tsv", result_file_name="merged.tsv"):
-    allFiles = glob.glob(path + pattern)
+    root_dir_path=os.path.dirname(sys.modules['__main__'].__file__)
+    allFiles = glob.glob(f"{root_dir_path}/{path}/{pattern}")
     allFiles.sort()
     print(f"Merging files: {allFiles}")
-    with open(f"{path}{result_file_name}", "wb") as outfile:
+    with open(f"{path}/{result_file_name}", "wb") as outfile:
         for i, fname in enumerate(allFiles):
             with open(fname, "rb") as infile:
                 if i != 0:
