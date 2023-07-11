f248b81a7906d881d7bfce56327f2dc4a9e99ec7
Latkos <48084189+Latkos@users.noreply.github.com>
2023-07-11 23:02:26 +0200

    Move function to evaluation utils

--- utils/evaluation.py
+++ utils/evaluation.py
@@ -1,3 +1,4 @@
+
 def evaluate_with_division_between_column(model, test_df, column_name, model_path=None, average_type="micro"):
     if model_path is None:
         model_path = model.model_path
@@ -11,3 +12,8 @@ def evaluate_with_division_between_column(model, test_df, column_name, model_pat
             test_df=lang_df, model_path=model_path, average_type=average_type
         )
     return evaluation_results
+
+def get_f1_from_metrics(metrics):
+    f1 = metrics["eval_overall_f1"]
+    print(f"f1: {f1}")
+    return f1
