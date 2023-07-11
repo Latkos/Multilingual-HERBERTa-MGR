c6a45c1e273009cc82ea0637284f490b8f4552c2
Latkos <48084189+Latkos@users.noreply.github.com>
2023-07-11 23:34:49 +0200

    Add model parameters to optuna reader

--- utils/optuna_reader.py
+++ utils/optuna_reader.py
@@ -1,3 +1,4 @@
+import ast
 import re
 import pandas as pd
 
@@ -6,6 +7,7 @@ def read_optuna_logs(file_name):
     trial_numbers = []
     metrics = []
     trial_runtimes = []
+    parameters=[]
     with open(file_name, encoding="utf-8") as file:
         for line in file:
             trial_number = re.search("Trial (.*) finished", line)
@@ -17,13 +19,18 @@ def read_optuna_logs(file_name):
             trial_runtime = re.search("'train_runtime': (.*), 'train_samples_per_second':", line)
             if trial_runtime:
                 trial_runtimes.append(trial_runtime.group(1))
-    df = pd.DataFrame(
-        data={
-            "trial_number": trial_numbers,
-            "metric": metrics,
-            "trial_runtime": trial_runtimes,
-        }
-    )
+            trial_parameters_match = re.search(r"parameters: ({.*})", line)
+            if trial_parameters_match is not None:
+                trial_parameters_str = re.search(r"parameters: ({.*})", line).group(1)
+                trial_parameters = ast.literal_eval(trial_parameters_str)
+                parameters.append(trial_parameters)
+    data = {
+        "trial_number": trial_numbers,
+        "metric": metrics,
+        "trial_runtime": trial_runtimes,
+    }
+    combined_data = {**data, **{key: [d[key] for d in parameters] for key in parameters[0]}}
+    df = pd.DataFrame(combined_data)
     df["metric"] = df["metric"].astype(float)
     df["trial_runtime"] = df["trial_runtime"].astype(float)
     return df
