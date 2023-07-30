45830c4e4c8e3a09dc1d3a35aa3ec7f3acdf34d8
Latkos <48084189+Latkos@users.noreply.github.com>
2023-07-30 13:49:38 +0200

    Fix typo in evaluation

--- utils/evaluation.py
+++ utils/evaluation.py
@@ -8,7 +8,7 @@ def evaluate_with_division_between_column(model, test_df, column_name, model_pat
     for unique_value in test_df[column_name].unique():
         print(f"{column_name}: {unique_value}")
         lang_df = test_df[test_df[column_name] == unique_value]
-        evaluation_results[column_name] = model.evaluate(
+        evaluation_results[unique_value] = model.evaluate(
             test_df=lang_df, model_path=model_path, average_type=average_type
         )
     return evaluation_results
