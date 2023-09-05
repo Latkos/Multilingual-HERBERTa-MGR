13f890eb82d5f04d95cbdcb2eacd3c263de744df
Latkos <48084189+Latkos@users.noreply.github.com>
2023-09-05 19:57:10 +0200

    Cleaning up code

--- utils/evaluation.py
+++ utils/evaluation.py
@@ -1,15 +1,13 @@
 
-def evaluate_with_division_between_column(model, test_df, column_name, model_path=None, average_type="micro"):
-    if model_path is None:
-        model_path = model.model_path
+def evaluate_with_division_between_column(model, test_df, column_name, average_type="micro"):
     print(f"COMMENCING EVALUATION DIVIDED BY {column_name} ")
     print(test_df[column_name].unique())
     evaluation_results = {}
     for unique_value in test_df[column_name].unique():
         print(f"{column_name}: {unique_value}")
-        lang_df = test_df[test_df[column_name] == unique_value]
+        subset_df = test_df[test_df[column_name] == unique_value]
         evaluation_results[unique_value] = model.evaluate(
-            test_df=lang_df, model_path=model_path, average_type=average_type
+            test_df=subset_df, average_type=average_type
         )
     return evaluation_results
 
--- utils/prediction.py
+++ utils/prediction.py
@@ -1,13 +1,13 @@
 import pandas as pd
 
 
-# def add_predictions_and_correctness_label_to_dataframe(model, test_df):
-#     original_entities_df = test_df.reset_index()
-#     sentences = test_df["text"].tolist()
-#     prediction_results = model.predict(sentences)
-#     results_df = pd.DataFrame(prediction_results)
-#     results_df = results_df.reset_index()
-#     return original_entities_df, results_df
+def add_predictions_and_correctness_label_to_dataframe(model, test_df):
+    original_entities_df = test_df.reset_index()
+    sentences = test_df["text"].tolist()
+    prediction_results = model.predict(sentences)
+    results_df = pd.DataFrame(prediction_results)
+    results_df = results_df.reset_index()
+    return original_entities_df, results_df
 
 
 def evalute_predictions_of_combined_models(test_df, ner_model, re_model):
