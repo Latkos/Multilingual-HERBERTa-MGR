5ae502482f1e08ce1b39090cc83b600468293469
Latkos <48084189+Latkos@users.noreply.github.com>
2023-09-24 15:28:57 +0200

    f1 fix to model size

--- experiments/model_size.py
+++ experiments/model_size.py
@@ -10,6 +10,10 @@ def test_model_quality_depending_on_dataset_size(model, train_df, test_df, sizes
         model_path = f"{model.__class__.__name__.lower()}_size_quality_tests_{size}"
         model.train(train_df=sampled_train_df, model_path=model_path)
         evaluation_result = model.evaluate(test_df, model_path)
-        results.append({"size": size, "f1": evaluation_result["eval_overall_f1"]})
+        print(f"EVAL RESULT LOOK: {evaluation_result}")
+        f1_column="f1"
+        if type(model) is NamedEntityModel:
+            f1_column="eval_overall_f1"
+        results.append({"size": size, "f1": evaluation_result[f1_column]})
     results_df = pd.DataFrame(results)
     return results_df
