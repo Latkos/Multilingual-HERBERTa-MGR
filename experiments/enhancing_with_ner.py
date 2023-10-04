0e462152317a171d45ea2e5b4b78bf0160174e95
Latkos <48084189+Latkos@users.noreply.github.com>
2023-10-04 22:01:21 +0200

    Better version on NER enhancing experiment

--- experiments/enhancing_with_ner.py
+++ experiments/enhancing_with_ner.py
@@ -1,27 +1,33 @@
-import pandas as pd
-from utils.enhancement import enhance_with_nothing, enhance_with_entity, enhance_with_brackets, enhance_with_entity_differentiated
+import torch
+from utils.enhancement import enhance_with_nothing, enhance_with_entity, enhance_with_brackets, \
+    enhance_with_entity_differentiated
+import pickle
 
+def save_results(filename, data):
+    with open(filename, 'wb') as fp:
+        pickle.dump(data, fp)
 
-def test_enhancing_text_used_to_train_re(train_df,test_df,ner_model,re_model):
-    results=ner_model.predict(sentences=train_df['text'].tolist())
-    input_no_enhance=enhance_with_nothing(results)
-    input_enhance_with_entity=enhance_with_entity(results)
-    input_enhance_with_entity_differentiated=enhance_with_entity_differentiated(results)
-    input_enhance_with_brackets=enhance_with_brackets(results)
+def train_and_evaluate(re_model, train_df, test_df, model_name):
+    print(f"Training {model_name}")
+    print(train_df.head(10)['text'].tolist())
+    re_model.train(train_df, model_path=f"models/{model_name}")
+    re_model.evaluate(df=test_df, model_path=f"models/{model_name}")
 
-    df_no_enhance = pd.DataFrame({'text': input_no_enhance})
-    df_entity = pd.DataFrame({'text': input_enhance_with_entity})
-    df_entity_differentiated = pd.DataFrame({'text': input_enhance_with_entity_differentiated})
-    df_brackets = pd.DataFrame({'text': input_enhance_with_brackets})
-    train_df_no_enhance = pd.concat([train_df.drop(columns='text'), df_no_enhance], axis=1)
-    train_df_entity = pd.concat([train_df.drop(columns='text'), df_entity], axis=1)
-    train_df_entity_differentiated = pd.concat([train_df.drop(columns='text'), df_entity_differentiated], axis=1)
-    train_df_brackets = pd.concat([train_df.drop(columns='text'), df_brackets], axis=1)
-    re_model.train(train_df_no_enhance, model_path='models/re_enhancing_no_enhance')
-    re_model.evaluate(df=test_df,model_path='models/re_enhancing_no_enhance')
-    re_model.train(train_df_entity, model_path='models/re_enhancing_entity')
-    re_model.evaluate(df=test_df,model_path='models/re_enhancing_entity')
-    re_model.train(train_df_entity_differentiated, model_path='models/re_enhancing_entity_differentiated')
-    re_model.evaluate(df=test_df,model_path='models/re_enhancing_entity_differentiated')
-    re_model.train(train_df_brackets, model_path='models/re_enhancing_brackets')
-    re_model.evaluate(df=test_df,model_path='models/re_enhancing_brackets')
+def test_enhancing_text_used_to_train_re(train_df, test_df, ner_model, re_model):
+    results = ner_model.predict(sentences=train_df['text'].tolist())
+    save_results('results_optimized_ner.txt', results)
+    # with open('results_optimized_ner.txt', 'rb') as file:
+    #     results = pickle.load(file)
+    enhancements = [
+        ("brackets", enhance_with_brackets),
+        ("no_enhance", enhance_with_nothing),
+        ("entity", enhance_with_entity),
+        ("entity_differentiated", enhance_with_entity_differentiated)
+    ]
+
+    for enhancement_name, enhancement_func in enhancements:
+        enhanced_train_df = train_df.copy()
+        enhanced_input = enhancement_func(results)
+        enhanced_train_df['text'] = enhanced_input
+        train_and_evaluate(re_model, enhanced_train_df, test_df, f"re_enhancing_{enhancement_name}_second_tryout")
+        torch.cuda.empty_cache()
