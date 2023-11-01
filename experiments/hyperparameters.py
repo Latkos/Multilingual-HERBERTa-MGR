7d11e732b58b26474f1ff0a703e814c147a230b4
Latkos <48084189+Latkos@users.noreply.github.com>
2023-11-01 18:51:00 +0100

    Remove test_parameters

--- experiments/hyperparameters.py
+++ experiments/hyperparameters.py
@@ -11,19 +11,9 @@ def optuna_hp_space_scientific(trial):
     return {
         "learning_rate": trial.suggest_categorical("learning_rate",[2e-5,3e-5,4e-5,5e-5]),
         "per_device_train_batch_size": trial.suggest_categorical("per_device_train_batch_size", [16]),
-        "num_train_epochs": trial.suggest_int("num_train_epochs", 1, 6),
+        "num_train_epochs": trial.suggest_categorical("num_train_epochs", [1]),
         "warmup_ratio": trial.suggest_categorical("warmup_ratio", [0.0,0.05,0.10, 0.15, 0.20]),
         "weight_decay": trial.suggest_categorical("weight_decay", [0.0,0.001,0.01,0.1,0.2,0.3]),
     }
 
 
-
-def test_hyperparameters(train_df, model, space=optuna_hp_space, config_path="./config/base_config.yaml",
-                         training_arguments=None, number_of_trials=50):
-    model.perform_hyperparameter_search(
-        train_df,
-        space,
-        config_path=config_path,
-        training_arguments=training_arguments,
-        number_of_trials=number_of_trials,
-    )
