10d4b95bcde8ad60bced0b66f8351794b8fa085c
Latkos <48084189+Latkos@users.noreply.github.com>
2023-11-01 18:17:58 +0100

    Add language sampling

--- experiments/sample_languages.py
+++ experiments/sample_languages.py
@@ -1,4 +1,8 @@
 import pandas as pd
+import torch
+
+from utils.enhancement import enhance_with_brackets
+from utils.preprocessing import filter_out_wrong_data
 
 
 def filter_and_set_proportions(df, proportions):
@@ -20,3 +24,30 @@ def filter_and_set_proportions(df, proportions):
             ])
 
     return df_filtered
+
+
+def train_and_evaluate_on_language_subsets(training_files, testing_files, ner_model, re_model,
+                                           enhance_function=enhance_with_brackets, average_type='micro',
+                                           skip_training=False):
+    train_dfs = [pd.read_csv(file, sep='\t') for file in training_files]
+    test_dfs = [pd.read_csv(file, sep='\t') for file in testing_files]
+    train_df = pd.concat(train_dfs, ignore_index=True)
+    test_df = pd.concat(test_dfs, ignore_index=True)
+    train_df = filter_out_wrong_data(train_df)
+    test_df = filter_out_wrong_data(test_df)
+    if len(train_df) >= 675000:
+        train_df = train_df.sample(n=675000)
+    language_names = '+'.join([file.split('_')[0] for file in training_files])
+    language_names=language_names.replace("data/","")
+    print(f"Language names: {language_names}")
+    if not skip_training:
+        ner_model.train(train_df, model_path=f"models/ner_{language_names}")
+    ner_model.evaluate(df=test_df, model_path=f"models/ner_{language_names}")
+    results = ner_model.predict(sentences=train_df['text'].tolist(), model_path=f"models/ner_{language_names}")
+    enhanced_input = enhance_function(results)
+    train_df['text'] = enhanced_input
+    if not skip_training:
+        re_model.train(train_df, model_path=f"models/re_{language_names}", remove_tags=False)
+    test_df = test_df[test_df['label'].isin(train_df['label'].unique())]
+    re_model.evaluate(df=test_df, model_path=f"models/re_{language_names}", average_type=average_type)
+    torch.cuda.empty_cache()
