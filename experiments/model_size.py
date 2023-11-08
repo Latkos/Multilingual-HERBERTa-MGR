056fa54a1c43feb1ea1c78e2b465052d2215c084
Latkos <48084189+Latkos@users.noreply.github.com>
2023-11-08 23:39:37 +0100

    Restructure model size functions

--- experiments/model_size.py
+++ experiments/model_size.py
@@ -1,17 +1,39 @@
 import pandas as pd
+import torch
 
 
-def test_model_quality_depending_on_dataset_size(model, train_df, test_df, sizes, random_state=None):
+def test_ner_quality_depending_on_dataset_size(model, train_df, test_df, sizes, random_state=None):
     results = []
     for size in sizes:
-        print(f"Testing {model.__class__.__name__.lower()}, size: {size}")
+        print(f"Testing NER, size: {size}")
+        if size>len(train_df):
+            print(f"Not enough training examples to train the model for size {size}")
+            continue
         sampled_train_df = train_df.sample(n=size, random_state=random_state)
-        model_path = f"../models/{model.__class__.__name__.lower()}_size_quality_tests_{size}"
+        model_path = f"../models/ner_size_quality_tests_{size}"
         model.train(train_df=sampled_train_df, model_path=model_path)
+        torch.cuda.empty_cache()
+        evaluation_result = model.evaluate(test_df, model_path)
+        f1 = evaluation_result["eval_overall_f1"]
+        results.append({"size": size, "f1": f1})
+    results_df = pd.DataFrame(results)
+    return results_df
+
+def test_re_quality_depending_on_dataset_size(model, train_df, test_df, sizes, random_state=None, remove_tags=False):
+    results = []
+    for size in sizes:
+        print(f"Testing RE, size: {size}")
+        if size>len(train_df):
+            print(f"Not enough training examples to train the model for size {size}")
+            continue
+        sampled_train_df = train_df.sample(n=size, random_state=random_state)
+        model_path = f"../models/re_size_quality_tests_{size}"
+        model.train(train_df=sampled_train_df, model_path=model_path)
+        torch.cuda.empty_cache()
         test_df_filtered = test_df.copy()
         test_df_filtered = test_df_filtered[test_df_filtered['label'].isin(sampled_train_df['label'].unique())]
         evaluation_result = model.evaluate(test_df_filtered, model_path)
-        f1_column = evaluation_result.get("eval_overall_f1", evaluation_result.get("f1"))
-        results.append({"size": size, "f1": evaluation_result[f1_column]})
+        f1 = evaluation_result["f1"]
+        results.append({"size": size, "f1": f1})
     results_df = pd.DataFrame(results)
     return results_df
