6ba534bf434062f93ce045c49aa8a7b15faea02c
Latkos <48084189+Latkos@users.noreply.github.com>
2023-11-08 23:40:25 +0100

    Add prints in case memory crashes or sth

--- experiments/enhancing_with_ner.py
+++ experiments/enhancing_with_ner.py
@@ -3,7 +3,7 @@ from utils.enhancement import enhance_with_nothing, enhance_with_entity, enhance
 from utils.prediction import train_re_on_ner
 
 
-def test_enhancing_text_used_to_train_re(train_df, test_df, ner_model, re_model,results_file='results_optimized_ner.txt',read=True):
+def test_enhancing_text_used_to_train_re(train_df, test_df, ner_model, re_model,results_file='results_optimized_ner.txt',read=True, train_ner=False):
     enhancements = [
         ("brackets", enhance_with_brackets),
         ("no_enhance", enhance_with_nothing),
@@ -12,9 +12,12 @@ def test_enhancing_text_used_to_train_re(train_df, test_df, ner_model, re_model,
         ("entity_with_special_characters", enhance_with_special_characters),
         ("entity_only", enhance_entities_only)
     ]
+    if train_ner:
+        ner_model.train(train_df=train_df)
     f1_values=[]
     for enhancement_name, enhancement_func in enhancements:
         result=train_re_on_ner(ner_model, re_model, train_df, test_df, enhancement_func, results_file, read, model_path=f"models/re_{enhancement_name}")
         f1=result['f1']
         f1_values.append(f1)
+        print(f"For enhancement {enhancement_name} the F1 is equal to {f1}")
     return f1_values
