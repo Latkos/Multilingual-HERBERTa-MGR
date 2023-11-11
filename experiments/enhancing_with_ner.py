b7a8eaf47e565a39f2aea1392220228bcc8f8e73
Latkos <48084189+Latkos@users.noreply.github.com>
2023-11-11 01:01:51 +0100

    Small enhancing fixes

--- experiments/enhancing_with_ner.py
+++ experiments/enhancing_with_ner.py
@@ -1,6 +1,6 @@
 from utils.enhancement import enhance_with_nothing, enhance_with_entity, enhance_with_brackets, \
     enhance_with_entity_differentiated, enhance_with_special_characters, enhance_entities_only
-from utils.prediction import train_re_on_ner
+from utils.prediction import train_re_on_ner, save_results
 
 
 def test_enhancing_text_used_to_train_re(train_df, test_df, ner_model, re_model,results_file='results_optimized_ner.txt',read=True, train_ner=False):
@@ -14,6 +14,8 @@ def test_enhancing_text_used_to_train_re(train_df, test_df, ner_model, re_model,
     ]
     if train_ner:
         ner_model.train(train_df=train_df)
+        results = ner_model.predict(sentences=train_df['text'].tolist())
+        save_results(results_file, results)
     f1_values=[]
     for enhancement_name, enhancement_func in enhancements:
         result=train_re_on_ner(ner_model, re_model, train_df, test_df, enhancement_func, results_file, read, model_path=f"models/re_{enhancement_name}")
